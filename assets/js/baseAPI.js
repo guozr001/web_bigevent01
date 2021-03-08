7d173276dfa89567bdd0187857d3d6ed854baf8c
”guozongrui <guozongrui1988@163.com>
2021-03-08 14:32:10 +0800

    wanchengle index

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,5 +1,21 @@
 //注意：每次调用$.get()或者$.post()或者$.ajax()的时候，会先调用这个函数。
 $.ajaxPrefilter(function(options){
+    
     //再发起真正的ajax之前，同意拼接请求的根路径
     options.url = 'http://ajax.frontend.itheima.net'+options.url
+    // 统一为有权限的接口设置header请求头
+    if (options.url.indexOf('/my/') !== -1){
+        options.headers = {
+            Authorization:localStorage.getItem('token')||""
+        }
+    }
+   //全局统一挂载complete回调函数
+   options.complete = function(res){
+        if(res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！'){
+        //1、强制清空token
+        localStorage.removeItem('token')
+        //2、强制跳转到登录页
+        location.href = 'login.html'
+        }
+   }
 })
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,27 +1,27 @@
 $(function(){
-    <!-- //点击去注册账号的链接 -->
+    // 点击去注册账号的链接
     $('#link_reg').on('click',function(){
         $('.login-box').hide()
         $('.reg-box').show();
     })
 
-    <!-- //点击去登陆的链接 -->
+    //点击去登陆的链接
     $('#link_login').on('click',function(){
         $('.reg-box').hide();
         $('.login-box').show();
     })
 
-    <!-- //从layui中获取form对象 -->
+    //从layui中获取form对象
     var form = layui.form;
     var layer = layui.layer;
-    <!-- //通过form.verify()函数自定义校验规则 -->
+    //通过form.verify()函数自定义校验规则
     form.verify({
-        <!-- //自定义一个pwd的校验规则 -->
+       //自定义一个pwd的校验规则
         pwd:[
         /^[\S]{6,12}$/
         ,'密码必须6到12位，且不能出现空格'
         ],
-        <!-- //校验两次密码是否一致 -->
+        //校验两次密码是否一致
         repwd:function(value){
             var pwd = $('.reg-box [name=password]').val();
             if(pwd != value){
@@ -30,7 +30,7 @@ $(function(){
         }
     })
 
-    <!-- //监听注册表单的提交事件 -->
+    //监听注册表单的提交事件
     $('#form_reg').on('submit',function(e){
         e.preventDefault();
         var data = {username: $('#form_reg [name=username]').val(),password:$('#form_reg [name=password]').val()};
@@ -40,13 +40,13 @@ $(function(){
     }
     
     layer.msg('注册成功,请登录！')
-    <!-- 模拟人的点击行为 -->
+    // 模拟人的点击行为
     $('#link_login').click();
 })
     })
     
 
-    <!-- 监听登陆表单的提交事件 -->
+    // <!-- 监听登陆表单的提交事件 -->
     
     $('#form_login').submit(function(e){
     e.preventDefault();
